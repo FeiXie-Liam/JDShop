5694533581792f179a98e1c31779f46107367eb5
FeiXie-Liam <xiefei0887@163.com>
2018-05-18 16:30:23 +0800

    add dependences.

---
+++ src/main/java/com/example/jdshop/JdshopApplication.java
@@ -0,0 +1,12 @@
+package com.example.jdshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdshopApplication.class, args);
+	}
+}
